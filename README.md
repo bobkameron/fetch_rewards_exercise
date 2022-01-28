cbc3ce0abfdb51b408ee256c0e0d87e91120fce4
Kevin Jin <bobkameron@gmail.com>
2022-01-28 14:06:17 -0600

    Added all unit tests. The API is fully functional already.

--- README.md
+++ README.md
@@ -1,14 +1,45 @@
 Fetch Rewards Points
 
-This project 
+This project allows someone to add points, spend points, and retrieve the balance of their points associated with a payer. The project uses the Django framework. To run the project, please install Django by entering into the command line:
+
+python -m pip install Django 
+
+Then, go into the mysite directory, and you can run the following command to start the web service:
+
+python manage.py runserver
+
+The default port is 8000; you can change this in mysite/mysite/settings.py. 
+
+You can access the admin page of the website at 
+
+http://localhost:8000/admin/
+
+The administrator login info is:
+username: admin
+password: Letuserin
+
+Here, you can manage payers and transactions.
+
+To run the unit tests, simply run  the command python manage.py test
+
+The tests for this app can be found inside mysite/points/tests.py 
+Feel free to add any further tests of your choosing. 
+
+All transactions are recorded in a durable data store (SQLite database). 
 
 
+API SPECIFICATION:
 
+The following is an API specification for the project- 
 
-Here is the specification of the API and routes for the project: 
+Once you start running the server, you can make HTTP API requests with curl or another
+utility of your choosing. The PUT and DELETE requests only accept requests where the body of the 
+request is in JSON format.
 
+The specification is in the format:  URL request-method, 
+where the request must be made to http://host:host_port/URL with the method request-method.
 
-/points/ GET 
+points/ GET 
 
 Any HTTP GET request simply returns a JSON object in an HTTP response body 
 that specifies the remaining balance for every payer that ever added 
@@ -22,7 +53,7 @@ left in the payer balance.
 HTTP status code of 200 is returned along with the HTTP response. 
 
 
-/points/ PUT 
+points/ PUT 
 
 A valid HTTP PUT request simply adds transactions for a specific payer and date. It should have the 'Content-Type' header set to 'application/json',
 and the body of the request should be a single JSON object in the format:
@@ -44,7 +75,7 @@ negative).
 Any valid HTTP JSON PUT request following the above specification will successfully add the transaction by adding the number of points specified at a timestamp associated with the payer, and return an HTTP response with status code 201. Otherwise, an HTTP response with error status code 4xx is returned indicating the failure to add the transaction. 
 
 
-/points/ DELETE
+points/ DELETE
 
 A valid HTTP DELETE request simply spends points. The 'Content-Type' header should be set to 'application/json', and the body of the request should be a single JSON object in the format:
 
@@ -52,7 +83,7 @@ A valid HTTP DELETE request simply spends points. The 'Content-Type' header shou
 
 Here, points should be a non-negative integer indicating the number of points that the user wants to spend. The number of points that the user wants to spend must be less than or equal to the total number of points that the user has left in their balance. 
 
-If the HTTP DELETE request follows the above specifications, then the points are spent in a manner where the oldest points are spent first (oldest based on transaction timestamp, not the order they’re received). An HTTP response of status code 204 is returned (200 if no points were spent if the client sends in a request of '{"points":0}' requesting no points to be spent ), and the body of the response is in the following format:
+If the HTTP DELETE request follows the above specifications, then the points are spent in a manner where the oldest points are spent first (oldest based on transaction timestamp, not the order they’re received). An HTTP response of status code 201 is returned (200 if no points were spent if the client sends in a request of '{"points":0}' requesting no points to be spent ), and the body of the response is in the following format:
 
 [
 { "payer": company_name1 , "points": points1 },
--- mysite/points/tests.py
+++ mysite/points/tests.py
@@ -7,34 +7,272 @@ from .views import index
 
 # Create your tests here.
 
+
+ 
+def get_balance(client):
+    return client.get(reverse("points"))
+
+  
+def add_transaction(client , payer, points, timestamp):
+    return client.put(reverse("points"),content_type='application/json', \
+            data = json.dumps({'payer':payer, 'points':points, 'timestamp': timestamp }))
+
+def spend( client, points):
+    return client.delete(reverse('points'), content_type='application/json',\
+        data = json.dumps({'points':points}))
+
 class TestPoints (TestCase):
-    '''
-    Testing stra
-    
-    
-    '''
 
 
-    def test_get(self):
+    '''
+    Unit Testing strategy: 
+
+    We will test each of the partitions in at least one test case, without guaranteeing that
+    each partition will be tested in its own test case. 
+
+    Testing partitions for HTTP GET (returning the balance), PUT (adding a new transaction), and 
+    DELETE (spending points) requests to the url '/points/':
+
+    Partitions for GET:
+
+        Partition by number of different payers in our database:
+        1. 0
+        2. 1
+        3. Multiple different payers 
+
+        Partition by value of each payer's point balance:
+        1. 0
+        2. A positive integer
+
+
+    Partitions for PUT:
+
+        Partition by if a transaction for same payer had previously been added:
+        1. True
+        2. False 
+
+        Partition by number of points in the transaction:
+        1. < 0 
+        2. = 0 (should return http error)
+        3. > 0 
+
+        Partition by the sum of new payer's point balance including the new transaction:
+        1. sum < 0 (should not add the transaction and instead return http error)
+        2. sum = 0 
+        3. sum > 0 
+        
+
+    Partitions for DELETE:
+
+        Partition by amount that is spent:
+        1. 0 points
+        2. Positive number of points. 
+
+        Partition by total sum of all payers' balances after spending the number of points:
+        1. Sum < 0 (points should not be spent and an error should be returned)
+        2. Sum = 0 
+        3. Sum > 0
+    
+
+    Testing functions strategy:
+
+    
+        Test no transactions
+
+        Test one added transaction with spends and balance requests (add, get, spend, get, spend, get ) - all valid requests 
+        
+        Test unsuccessfully adding a transaction
+
+        Test unsuccessfully spending more than the sum of all payers' balance 
+
+        Test multiple valid transactions added to the same payer
+
+        Test multiple valid transactions added to different payers 
+
+    '''
+
+    def test_no_transactions(self):
         c = Client() 
-        response = c.get(reverse("points"))
+        response = get_balance(c)
         self.assertEqual(response.status_code, 200 )
 
-        print(response.content)
-
         data = json.loads( response.content )
-        print(data)
         self.assertEqual (len(data), 0 )
 
-    def test_put(self):
+    def test_one_transaction(self):
         c = Client()
         
-        response = c.put(reverse("points"),content_type='application/json', \
-            data = "  " )
+        payer = 'dannon'
+        amount = 1000
 
-        print(response.content)
-    
-       
+        response_add = add_transaction(c, payer , amount,"2020-11-02T14:00:00Z" ) 
+        result_add = get_balance(c)
+
+        self.assertEqual(response_add.status_code, 201)
+
+        data = json.loads(result_add.content)
+
+        self.assertEqual(len(data),1 )
+        self.assertEqual ( data[payer], amount )
+
+        spend_nothing = spend(c, 0)
+        self.assertEqual(spend_nothing.status_code, 200)
+        data1 = json.loads(get_balance(c).content)
+        self.assertEqual( data1[payer], amount )
         
+
+        spend_amount = 500
+        response_delete = spend(c, spend_amount )
+        self.assertEqual(response_delete.status_code, 201)
         
+        data_delete = json.loads(response_delete.content)
+
+        self.assertEqual( len(data_delete), 1 )
+        self.assertEqual(data_delete[0], { "payer": payer, "points": -spend_amount }, )
+        
+        response_spend_all = spend(c, spend_amount)
+        self.assertEqual(response_spend_all.status_code, 201 )
+        data_spend_all = json.loads(response_spend_all.content)
+
+        self.assertEqual(len(data_spend_all), 1)
+        self.assertEqual( data_spend_all[0], { "payer": payer, "points": -spend_amount }  )
+
+        result_spend_all = get_balance(c)
+        get_data = json.loads(result_spend_all.content)
+        self.assertEqual(len(get_data),1 )
+        self.assertEqual(get_data[payer], 0)
     
+    def test_invalid_add_transaction(self):
+        c = Client()
+        payer = 'dannon'
+        amount = 1000
+        timestamp1 = "2020-11-02T14:00:00Z"
+        timestamp2 = "2020-12-03T14:00:00Z"
+
+        _ = add_transaction(c, payer , amount,timestamp1 )
+        invalid_add = add_transaction( c, payer, - (amount * 2), timestamp2)
+        print(invalid_add.status_code, 'invalid add transaction status code')
+
+        self.assertTrue(  399 <  invalid_add.status_code < 500 )
+        final_balance = get_balance(c)
+        data = json.loads(final_balance.content)
+        self.assertTrue( len(data) == 1 and data[payer] == amount  )
+
+        invalid_add_zero = add_transaction(c, payer, 0, timestamp2)
+        self.assertTrue(399 < invalid_add_zero.status_code < 500)
+        data = json.loads(get_balance(c).content)
+        self.assertTrue (len(data) == 1 and data[payer] == amount)
+        
+    def test_invalid_spend (self):
+        c = Client()
+        payer = 'toyota'
+        amount = 2000
+        timestamp1 = "2021-11-02T14:00:00Z"
+
+        invalid_spend =  spend ( c, amount)
+        self.assertTrue ( 399 <  invalid_spend.status_code < 500 )
+        _ = add_transaction(c, payer, amount, timestamp1 )
+        
+        invalid_spend2 = spend (c, amount + 1)
+        self.assertTrue(399 < invalid_spend2.status_code < 500)
+
+        result = get_balance(c)
+        data = json.loads(result.content)
+        self.assertTrue(len(data) == 1 and data [payer] == amount)
+
+    def test_multiple_valid_add_transactions_same_payer(self):
+        c = Client()
+        payer = 'toyota'
+        amount = 2000
+        timestamp1 = "2021-11-02T14:00:00Z"
+        timestamp2 = "2022-12-02T14:00:00Z"
+
+        add_transaction ( c, payer, amount, timestamp1)
+        add_transaction ( c, payer, amount, timestamp2)
+        add_transaction ( c, payer, amount, timestamp1)
+
+        result = get_balance(c)
+        data = json.loads(result.content)
+
+        self.assertTrue( len(data) == 1 and data['toyota'] == amount * 3)
+
+        result_spend = spend(c, amount * 3)
+        data_spend = json.loads(result_spend.content)
+
+        self.assertTrue(result_spend.status_code == 201 and len(data_spend) == 1 \
+            and data_spend[0]['payer'] == payer and data_spend[0]['points'] == - 3 * amount   )
+
+        result_final = get_balance(c)
+        data= json.loads(result_final.content)
+        print(data, '  data  ')
+        self.assertTrue (len(data) == 1 and data[payer] == 0 )
+
+    def test_multiple_valid_add_transactions_different_payers(self):
+        c = Client() 
+
+        payer1 = 'DANNON'
+        payer2 = 'UNILEVER'
+        payer3 = 'MILLER COORS'
+
+        payers = [ payer1, payer2, payer1, payer3, payer1 ]
+        
+
+        timestamp1 = "2020-11-02T14:00:00Z"
+        timestamp2 = "2020-10-31T11:00:00Z"
+        timestamp3 = "2020-10-31T15:00:00Z" 
+        timestamp4 = "2020-11-01T14:00:00Z"
+        timestamp5 = "2020-10-31T10:00:00Z"
+        
+        timestamps = [ timestamp1, timestamp2, timestamp3, timestamp4, timestamp5 ]
+        points = [ 1000, 200, -200 , 10000, 300]
+
+        for i in range (len(payers)):
+            add_transaction(c,payers[i], points[i], timestamps[i])
+        
+        spend_amount = 5000 
+        result_spend = spend(c, spend_amount )
+
+        expected = [
+            { "payer": "DANNON", "points": -100 },
+            { "payer": "UNILEVER", "points": -200 },
+            { "payer": "MILLER COORS", "points": -4700 }
+        ]
+
+        self.assertEquals(result_spend.status_code, 201)
+        data_spend = json.loads(result_spend.content)
+        self.assertEquals( len(data_spend), 3)
+
+        for dic in expected:
+            assert( dic in data_spend)
+
+        balance = get_balance(c)
+        data = json.loads(balance.content)
+        self.assertEquals(data, {
+            "DANNON": 1000,
+            "UNILEVER": 0,
+            "MILLER COORS": 5300
+        })
+
+        spend_amount2 = 6300 
+        result_spend2 = spend(c, spend_amount2 )
+
+        expected2 = [
+            { "payer": "DANNON", "points": -1000 },
+            { "payer": "MILLER COORS", "points": -5300 }
+        ]
+
+        data_spend2 = json.loads(result_spend2.content)
+        self.assertEquals(len(data_spend2), 2)
+        for dic in expected2:
+            assert (dic in data_spend2)
+
+        balance2 = get_balance(c)
+        data2 = json.loads(balance2.content)
+        self.assertEquals(data2, {
+            "DANNON": 0,
+            "UNILEVER": 0,
+            "MILLER COORS": 0
+        })
+
+
+        
